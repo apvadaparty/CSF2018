64bc861d0e71f4532af70f00b1c42845f66400de
Live session user <apvadaparty@gmail.com>
2018-03-21 18:52:00 -0400

    thirtytwo.s

--- thirtytwo.s
+++ thirtytwo.s
@@ -1,3 +1,5 @@
+;Annapurna Vadaparty
+;avadapa2@jhu.edu
 ; 32 bit little endian addition and subtraction
 ;
 ; write the two subroutines below and make sure they
@@ -23,6 +25,23 @@ main:
 ; $04..$07 into a 32 bit result stored at $08..$0b
 add32:
 	; your code here
+	CLC ; in case already exists carry from previous instruction
+	lda $00
+	adc $04 
+	sta $08
+	;next digit
+	lda $01
+	adc $05 
+	sta $09
+	;next digit 
+	lda $02
+	adc $06 
+	sta $0a
+	;next digit
+	lda $03
+	adc $07 
+	sta $0b
+
 	rts
 
 ; subtract the 32 bit integer stored at $04..$07 from
@@ -30,4 +49,20 @@ add32:
 ; 32 but result into $0c..$0f
 sub32:
 	; your code here
+	SEC
+	lda $00
+	sbc $04 
+	sta $0c
+	;next digit
+	lda $01
+	sbc $05 
+	sta $0d
+	;next digit 
+	lda $02
+	sbc $06 
+	sta $0e
+	;next digit
+	lda $03
+	sbc $07 
+	sta $0f
 	rts
